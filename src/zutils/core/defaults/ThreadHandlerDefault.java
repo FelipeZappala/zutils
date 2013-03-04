4b2b39444c30c5f7da5aba83b6bbce9536920b95
Felipe Zappala <zappala@MacZap.local>
2013-03-03 23:33:56 -0300

    Adicionado pool de execucao de threads

--- src/zutils/core/defaults/ThreadHandlerDefault.java
+++ src/zutils/core/defaults/ThreadHandlerDefault.java
@@ -1,32 +1,51 @@
 package zutils.core.defaults;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import zutils.core.ThreadHandler;
 import zutils.core.exceptions.ZUtilsException;
 
 class ThreadHandlerDefault implements ThreadHandler {
 
+	private ExecutorService pool;
+	
+	public ThreadHandlerDefault() {
+		this.pool = Executors.newFixedThreadPool(10);
+	}
+	
 	public ThreadHandler execute(Runnable function) {
-		return execute(function, false);
+		//return execute(function, false);
+		
+		this.pool.execute(function);
+		return this;
 	}
 
 	public ThreadHandler await(Runnable function) {
-		return execute(function, true);
-	}
-
-	private ThreadHandler execute(Runnable function, boolean await) {
+		//return execute(function, true);
+		
 		try {
-			if (function != null) {
-				Thread thread = new Thread(function);
-				thread.start();
-				
-				if (await) {
-					thread.join();
-				}
-			}
+			this.pool.submit(function).get();
 		} catch (Exception e) {
 			throw new ZUtilsException(e);
 		}
 		return this;
 	}
+
+//	private ThreadHandler execute(Runnable function, boolean await) {
+//		try {
+//			if (function != null) {
+//				Thread thread = new Thread(function);
+//				thread.start();
+//				
+//				if (await) {
+//					thread.join();
+//				}
+//			}
+//		} catch (Exception e) {
+//			throw new ZUtilsException(e);
+//		}
+//		return this;
+//	}
 	
 }
